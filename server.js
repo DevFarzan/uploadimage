152bc3e34692347885fe926464641cd6d2507fb3
DevFarzan <farzanhanif123@gmail.com>
2018-07-17 16:28:10 +0500

    server

--- server.js
+++ server.js
@@ -1,47 +1,17 @@
-   express = require('express');
-      //config      = require('./config'),
-      mongoose    = require('mongoose'),
-      util = require('util')
-
-
- app = express();
- port = process.env.PORT || 5000;
-
- app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http'){
-    next();
-  }else{
-    res.redirect('http://' + req.hostname + req.url);
-  }
-});
-//Connecting database
- app.use(function(req, res, next) {
-
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Credentials", "true");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-        next();
-    });
- configDB = require('./config/database.js');
-mongoose.connect(configDB.EvenNodeDB);
-
- db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Database connection failed:'));
-db.once('open', function (callback) {
-    console.log("Database :: PakJazba :: connection established successfully.");
-});
-
-
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+// API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
-
-app.get('/api/upload',(req, res) =>{
-	console.log('--------------'+JSON.stringify(req.query.image)+'-----------------');
-	//var image = req.body.image;
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify(req.query.image.name))
-});
-
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 app.listen(port, () => console.log(`Listening on port ${port}`));
